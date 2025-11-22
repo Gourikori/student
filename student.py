c3546ffc3bbfd893a98da4ff2e9fcff9a14c54c4
Gourikori <gourikori425@gmail.com>
2025-11-22 09:27:48 +0530

    student.py

---
+++ student.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) 1=3:
+print("usage:python student.py <name">
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
